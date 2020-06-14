2dc1161545da32da017560c1264b1f6367662d2f
Ropuia1234 <ctvanlalropuia@gmail.com>
2020-06-14 11:29:33 -0700

    End of Section 8 (final)

--- MizoChoka-SPA/src/routes.ts
+++ MizoChoka-SPA/src/routes.ts
@@ -2,15 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './app/home/home.component';
 import { EiturBuatsaihDanComponent } from './app/EiturBuatsaihDan/EiturBuatsaihDan.component';
 import { AuthGuard } from './app/_guards/auth.guard';
+import { DahbelhnaComponent } from './app/Dahbelhna/Dahbelhna.component';
 
 export const appRoutes: Routes = [
     {path: '', component: EiturBuatsaihDanComponent },
+    {path: 'home', component: HomeComponent},
     {
         path: '',
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            {path: 'home', component: HomeComponent},
+            {path: 'Dahbelhna', component: DahbelhnaComponent},
         ]
     },
       {path: '**', redirectTo: '', pathMatch: 'full'},
